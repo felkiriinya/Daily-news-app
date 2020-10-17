7b13207266beb2fccb3f81fc940325195d49abd4
felkiriinya <felkiriinya@gmail.com>
2020-10-17 15:03:53 +0300

    added configurations

---
+++ app/main/views.py
@@ -0,0 +1,11 @@
+# from flask import render_template
+# from . import main
+
+# # Views
+# @main.route('/')
+# def index():
+
+#     '''
+#     View root page function that returns the index page and its data
+#     '''
+#     return render_template('index.html')
