444d46dcfd2d893fc220edcd8d9a2cd478f44a19
felkiriinya <felkiriinya@gmail.com>
2020-10-17 16:11:49 +0300

    made the first API source call and proccessed results

--- app/request.py
+++ app/request.py
@@ -1,5 +1,56 @@
 from app import app
+import urllib.request,json
+from .models import source
+
+Source = source.Source
 
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the movie base url
+base_url = app.config['NEWS_SOURCES_BASE_URL']
+
+
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['results']:
+            sources_results_list =  get_sources_response['results']
+            source_results = process_results(sources_results_list)
+
+    return source_results    
+
+def process_results(sources_list):
+    '''
+    Function that processes the news sources results and turns them into a list of objects
+    Args:
+            sources_list: A list of dictionaries that contain sources details
+    Returns:
+            source_results: A list of sources objects
+    '''  
+    source_results = []
+    for source_item in sources_list:
+    
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        country = source_item.get('country')
+        language = source_item.get('language')
+
+        sources_object = Source(id,name,description,url,category,country,language)
+
+        source_results.append(sources_object)
+
+    return source_results    
--- app/source_test.py
+++ app/source_test.py
@@ -12,16 +12,16 @@ class SourceTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_source = Source('CNN','CNN News','Cable News Newtork that is a leader in providing news worldwide','cnn.com','general','U.S.A','en')
+        self.new_source = Source('Bloomberg','Bloomberg News','Bloomberg delivers business and markets news, data, analysis, and video to the world, featuring','bloomberg.com','general','U.S.A','en')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Source))
 
     def test_to_check_instance_variables(self):
-        self.assertEquals(self.new_source.id,'CNN')
-        self.assertEquals(self.new_source.name,'CNN News')
-        self.assertEquals(self.new_source.description,'Cable News Newtork that is a leader in providing news worldwide')
-        self.assertEquals(self.new_source.url,'cnn.com')
+        self.assertEquals(self.new_source.id,'Bloomberg')
+        self.assertEquals(self.new_source.name,'Bloomberg News')
+        self.assertEquals(self.new_source.description,'Bloomberg delivers business and markets news, data, analysis, and video to the world, featuring')
+        self.assertEquals(self.new_source.url,'bloomberg.com')
         self.assertEquals(self.new_source.category,'general')
         self.assertEquals(self.new_source.country,'U.S.A')
         self.assertEquals(self.new_source.language,'en')
