d6703eca2dc3b407de4120717bb16f8824c9efa0
felkiriinya <felkiriinya@gmail.com>
2020-10-18 19:22:28 +0300

    added the models classes file

---
+++ app/models.py
@@ -0,0 +1,26 @@
+class Source:
+    '''
+    Sources class to define Sources Objects
+    '''
+
+    def __init__(self,id,name,description,url,category,country,language):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        self.language = language
+
+class Articles:
+    '''
+    Articles class to define articles objects
+    '''
+    def __init__(self,id,author,title,description,url,image,date):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image = image
+        self.date = date         
