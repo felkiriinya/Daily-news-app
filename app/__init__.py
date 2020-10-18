34d5a517a6b1775d0e3ae16ccfd59a51bdc54333
felkiriinya <felkiriinya@gmail.com>
2020-10-18 19:43:52 +0300

    created an application instance

--- app/__init__.py
+++ app/__init__.py
@@ -17,16 +17,3 @@ def create_app(config_name):
     # Will add the views and forms
 
     return app
-# Initializing application
-app = Flask(__name__,instance_relative_config=True)
-
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
-
-
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
-
-from app import views
-from app import error
--- app/request.py
+++ app/request.py
@@ -1,22 +1,27 @@
-from app import app
+
 import urllib.request,json
-from .models import source
-from .models import article
+from .models import Source,Articles
 
-Source = source.Source
-
-Articles = article.Articles
 
 
 # Getting api key
-api_key = app.config['NEWS_API_KEY']
+
+api_key = None
 
 # Getting the movie base url
-base_url = app.config['NEWS_SOURCES_BASE_URL']
+
+base_url = None
 
 #Getting the articles base url
-articles_url = app.config['ARTICLES_BASE_URL']
 
+articles_url = None
+
+def configure_request(app):
+    global api_key,base_url,articles_url
+    
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_SOURCES_BASE_URL']
+    articles_url = app.config['ARTICLES_BASE_URL']
 
 def get_sources(category):
     '''
