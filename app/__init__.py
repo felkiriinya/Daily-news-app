333442c79a1f43b21e919536a29c2dfe3b21d7bf
felkiriinya <felkiriinya@gmail.com>
2020-10-18 19:08:37 +0300

    function to allow app to be launched from the script

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,22 @@
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
 
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
 # Initializing application
 app = Flask(__name__,instance_relative_config=True)
 
